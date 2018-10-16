e9d0f71913a654455c4cfed79292721bd8f18c74
Harry Daninthe <DDHarry@users.noreply.github.com>
2018-10-16 21:19:13 +0200

    Update variable_switch.py

--- 00_Basic_Features/variable_switch.py
+++ 00_Basic_Features/variable_switch.py
@@ -1,10 +1,11 @@
 #
 # switching 2 or more variables
 va =1
-vb = 2; vc = 3; # We can write many more ones
-va, vb, vc = 4,5,6
-print("va : ",va)
-print("vc : ",vc)
+vb = 2; vc = 3; # Not very Python ;-)
+
+va, vb, vc = 4,5,6 # We can write many more ones
+print("va = ",va, " vb = ", vb)
+print("vc = ",vc)
 
 va, vb, vc = vc, vb, va
 print("And now, ve have switched the variables")
