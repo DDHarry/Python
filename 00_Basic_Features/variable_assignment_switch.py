ed57eb76018a283645e5e1a887d7c37a898aebf6
Harry Daninthe <DDHarry@users.noreply.github.com>
2018-10-17 17:06:17 +0200

    Update and rename variable_assignement_switch.py to variable_assignment_switch.py

---
+++ 00_Basic_Features/variable_assignment_switch.py
@@ -0,0 +1,14 @@
+#
+# Not very Python assignment;-)
+va =1
+vb = 2; vc = 3;
+print("va is : ",va); print("vc is now : ",vc)
+
+# Assignment in idiomatic Python
+va, vb, vc = 4,5,6
+print("va = ",va,"; vb = ", vb, "; vc = ",vc,".")
+
+# switching 2 or more variables: the Python's way
+va, vb, vc = vc, vb, va
+print("And now, ve have switched the variables")
+print("va is now : ",va,"; vb is now : ",vb, "; vc is now : ",vc)
